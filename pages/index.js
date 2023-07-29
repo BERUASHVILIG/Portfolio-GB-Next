57750b7b5e987ae0f68da86e3fb1eee8c64ed290
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2023-07-29 13:18:49 +0400

    header particles

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,13 @@
 // next image
 import Image from "next/image";
 
-// components
 import ParticlesContainer from "../components/ParticlesContainer";
 import ProjectsBtn from "../components/ProjectsBtn";
 import Avatar from "../components/Avatar";
 
-// framer motion
-import { easeInOut, motion } from "framer-motion";
+// framer
 
-// variants
+import { motion } from "framer-motion";
 
 import { fadeIn } from "../variants";
 
@@ -17,8 +15,7 @@ const Home = () => {
   return (
     <div className="bg-primary/60 h-full">
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
-        <div className=" text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
-          {/* title */}
+        <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto">
           <motion.h1
             variants={fadeIn("down", 0.2)}
             initial="hidden"
@@ -50,26 +47,24 @@ const Home = () => {
             initial="hidden"
             animate="show"
             exit="hidden"
-            className="hidden xl:flex"
+            className="hidden xl:flex "
           >
             <ProjectsBtn />
           </motion.div>
         </div>
+      </div>
+      <div className="w-[1200px] h-full absolute right-0 bottom-0">
+        <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
+        <ParticlesContainer />
         <motion.div
-          variants={fadeIn("up", 0.5)}
+          variants={fadeIn("up", 0.2)}
           initial="hidden"
           animate="show"
           exit="hidden"
           transition={{ duration: 1, ease: "easeInOut" }}
-          className="w-[1200px] h-full absolute right-0 bottom-0"
+          className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]"
         >
-          <div className="bg-none xl:bg-explosion xl:bg-cover xl:bg-right xl:bg-no-repeat w-full h-full absolute mix-blend-color-dodge translate-z-0"></div>
-          {/* particles */}
-          <div>particles</div>
-          {/* avatar img */}
-          <div className="w-full h-full max-w-[737px] max-h-[678px] absolute -bottom-32 lg:bottom-0 lg:right-[8%]">
-            <Avatar />
-          </div>
+          <Avatar />
         </motion.div>
       </div>
     </div>
