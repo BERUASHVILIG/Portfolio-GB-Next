935c12c6e0a5e136b21ba5f389c6fc191b67908e
giorgi beruashvili <giorgiberuashvili112@gmail.com>
2023-08-16 20:03:04 +0400

    add project data

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -1,23 +1,38 @@
-// Data
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import { Pagination } from "swiper";
+import { BsArrowRight } from "react-icons/bs";
+
 const workSlides = {
   slides: [
     {
       images: [
         {
-          title: "title",
-          path: "./thumb1.jpg",
+          title: "Promptopia",
+          path: "./promptopia-img.png",
+          githubUrl: "https://github.com/BERUASHVILIG/Promptopia-Project",
+          liveUrl: "https://promptopia-project-omega.vercel.app/",
         },
         {
-          title: "title",
-          path: "./thumb2.jpg",
+          title: "Ecommerce",
+          path: "./flair-img.png",
+          githubUrl: "https://github.com/BERUASHVILIG/Next.js-eCommerce",
+          liveUrl: "https://next-js-e-commerce-ezq1.vercel.app/",
         },
         {
-          title: "title",
-          path: "./thumb3.jpg",
+          title: "Book-Store",
+          path: "./bookStore-img.png",
+          githubUrl: "https://github.com/BERUASHVILIG/Book-App-Redux",
+          liveUrl: "https://book-app-redux-ejhhc6x7i-beruashvilig.vercel.app/",
         },
         {
-          title: "title",
-          path: "./thumb4.jpg",
+          title: "AI-Summarizer",
+          path: "./summarize-img.png",
+          githubUrl: "https://github.com/BERUASHVILIG/AI-Summarizer",
+          liveUrl: "https://ai-summarizer-lovat-eight.vercel.app/",
         },
       ],
     },
@@ -27,33 +42,10 @@ const workSlides = {
           title: "title",
           path: "./thumb4.jpg",
         },
-        {
-          title: "title",
-          path: "./thumb1.jpg",
-        },
-        {
-          title: "title",
-          path: "./thumb2.jpg",
-        },
-        {
-          title: "title",
-          path: "./thumb3.jpg",
-        },
       ],
     },
   ],
 };
-
-import { Swiper, SwiperSlide } from "swiper/react";
-
-import "swiper/css";
-import "swiper/css/free-mode";
-import "swiper/css/pagination";
-
-import { Pagination } from "swiper";
-
-import { BsArrowRight } from "react-icons/bs";
-import Image from "next/image";
 
 const WorkSlider = () => {
   return (
@@ -80,7 +72,14 @@ const WorkSlider = () => {
                       <div className="absolute inset-0 bg-gradient-to-l from-transparent via-[#e838cc] to-[#4a22bd] opacity-0 group-hover:opacity-80 transition-all duration-700"></div>
                       <div className="absolute bottom-0 translate-y-full group-hover:-translate-y-10 group-hover:xl:-translate-y-20 transition-all duration-300">
                         <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-                          <div className="delay-100">LIVE</div>
+                          <a
+                            href={image.liveUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            className="delay-100"
+                          >
+                            LIVE
+                          </a>
                           <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
                             Project
                           </div>
@@ -89,7 +88,13 @@ const WorkSlider = () => {
                           </div>
                         </div>
                         <div className="flex items-center gap-x-2 text-[13px] tracking-[0.2em]">
-                          <div className="delay-100">Github</div>
+                          <a
+                            href={image.githubUrl}
+                            target="_blank"
+                            rel="noopener noreferrer"
+                          >
+                            Github
+                          </a>
                           <div className="translate-y-[500%] group-hover:translate-y-0 transition-all duration-300 delay-150">
                             Link
                           </div>
